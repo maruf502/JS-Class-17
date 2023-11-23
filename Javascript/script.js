21cc3e36ddd30b34b1e28d61d1e8fe989e4dd8f4
Maruf Khan <marufatrai12@gmail.com>
2023-11-23 10:40:35 +0600

    JS-Class-17

---
+++ Javascript/script.js
@@ -0,0 +1,51 @@
+
+
+
+
+    //  print the Number for loop in JavaScript 1 to 100;
+
+
+/*
+
+    for (var x = 1 ; x <= 100; x++){
+
+        document.write(x + "<br/>");
+
+    }
+
+*/
+
+    //  print the Number for loop in JavaScript 100 to 1;
+
+/*
+
+    for (var x = 100; x >= 1 ; x--){
+
+        document.write(x + "<br/>");
+
+    }
+
+*/
+
+
+    //  print the Number for loop in JavaScript odd number;
+
+
+/*
+
+    for (var x = 1; x <= 100; x+=2){
+
+        document.write(x + "<br>");
+
+    }
+
+*/
+
+
+    //  print the Number for loop in JavaScript Even number;
+
+    for(var x = 2; x <= 100; x = x + 2){
+
+        document.write(x + "<br/>");
+
+    }
